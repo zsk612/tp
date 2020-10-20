9fe2d52e5c9064d20ac1ebe933ed43065f14b3e8
CFZeon <e0406826@u.nus.edu>
2020-10-20 11:57:45 +0800

    Fix clear command, user input in diet manager

--- src/main/java/commands/diet/dietmanager/DietSessionClear.java
+++ src/main/java/commands/diet/dietmanager/DietSessionClear.java
@@ -1,25 +1,25 @@
 package commands.diet.dietmanager;
 
 import commands.Command;
-import diet.dietsession.DietSession;
 import storage.diet.Storage;
-import ui.diet.dietmanager.DietManagerUi;
 
 import java.io.File;
-import java.io.IOException;
-import java.sql.SQLOutput;
-import java.util.Arrays;
 import java.util.Objects;
 
 public class DietSessionClear extends Command {
-//    static final String FILEPATH = "saves/diet/";
-//    @Override
-//    public void execute(String input, Storage storage) {
-//        File folder = new File(FILEPATH);
-//        File[] listOfFiles = folder.listFiles();
-//        for(int index = 0; index <= Objects.requireNonNull(listOfFiles).length; index++) {
-//            listOfFiles[index].delete();
-//        }
-//        System.out.println("Oops You have cleared all diet sessions");
-//    }
+    static final String FILEPATH = "saves/diet/";
+
+    @Override
+    public void execute(String input, Storage storage) {
+        try {
+            File folder = new File(FILEPATH);
+            File[] listOfFiles = folder.listFiles();
+            for (int index = 0; index < Objects.requireNonNull(listOfFiles).length; index++) {
+                listOfFiles[index].delete();
+            }
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+        System.out.println("You have cleared all diet sessions!");
+    }
 }
