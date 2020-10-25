842e4cf4af0dc7481c4061b4a441b8d6beb1aebf
tienkhoa16 <nguyentienkhoa22@gmail.com>
2020-10-25 21:48:45 +0800

    Update profile tests

---
+++ src/main/java/exceptions/profile/InvalidCaloriesException.java
@@ -0,0 +1,18 @@
+package exceptions.profile;
+
+import exceptions.SchwarzeneggerException;
+
+import static profile.Constants.MESSAGE_INVALID_CALORIES;
+
+/**
+ * Represents exception when input calories is invalid.
+ */
+public class InvalidCaloriesException extends SchwarzeneggerException {
+
+    /**
+     * Constructs InvalidCaloriesException object inheriting abstract class SchwarzeneggerException.
+     */
+    public InvalidCaloriesException() {
+        super(MESSAGE_INVALID_CALORIES);
+    }
+}
