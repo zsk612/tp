eda308ec0cb7c1e98090229ac7781656cbfd9cf1
tienkhoa16 <nguyentienkhoa22@gmail.com>
2020-11-02 20:21:51 +0800

    Add MainEnd class

---
+++ src/main/java/commands/main/MainEnd.java
@@ -0,0 +1,30 @@
+package commands.main;
+
+import commands.Command;
+import commands.CommandResult;
+import exceptions.EndException;
+import exceptions.SchwarzeneggerException;
+
+/**
+ * A representation of the command for exiting The Schwarzenegger.
+ */
+public class MainEnd extends Command {
+
+    /**
+     * Overrides execute method of class Command to execute end command requested by user's input.
+     *
+     * @param args User's input.
+     * @return Result of command execution.
+     * @throws SchwarzeneggerException If there are caught exceptions.
+     */
+    @Override
+    public CommandResult execute(String args) throws SchwarzeneggerException {
+        super.execute(args);
+
+        if (!args.isEmpty()) {
+            ui.showWarning("\"end\" command does not take in parameters");
+        }
+
+        throw new EndException();
+    }
+}
