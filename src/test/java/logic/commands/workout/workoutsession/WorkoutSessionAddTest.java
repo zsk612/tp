cee44bc90c57acedaafd02a96d5d320d8e659b48
yujinyang1998 <yujinyang1998@users.noreply.github.com>
2020-11-07 04:23:04 +0800

    Add test for workoutsession parser and add command

--- src/test/java/logic/commands/workout/workoutsession/WorkoutSessionAddTest.java
+++ src/test/java/logic/commands/workout/workoutsession/WorkoutSessionAddTest.java
@@ -1,12 +1,30 @@
 package logic.commands.workout.workoutsession;
 
+import logic.commands.CommandResult;
+import models.ExerciseList;
+import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.junit.jupiter.api.Test;
+import storage.workout.Storage;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class WorkoutSessionAddTest {
 
     @Test
-    void execute() {
+    void execute_validInput_success() {
+        String[] inputs = {"add", "bench", "/n", "3244", "/w", "4324"};
+        ExerciseList exerciseList = new ExerciseList();
+        String filePath = "./saves/workout/workoutSession0";
+        Storage storage = new Storage();
+        boolean[] hasEndedWorkoutSessions = {false};
+
+        WorkoutSessionAdd workoutSessionAdd = new WorkoutSessionAdd();
+
+        CommandResult actual = workoutSessionAdd.execute(inputs, exerciseList, filePath, storage,
+                hasEndedWorkoutSessions);
+        CommandResult expected = new CommandResult("Yay! You have added bench to your list.\n"
+                + "\t [Repetitions: 3244 || Weight: 4324]");
+        assertTrue(EqualsBuilder.reflectionEquals(expected, actual));
     }
 
 }
