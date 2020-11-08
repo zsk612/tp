7c123648117b0600ce2acaa33b708e1101bbec5c
tienkhoa16 <nguyentienkhoa22@gmail.com>
2020-11-08 19:11:49 +0800

    Refactor magic number and string in profile parser

--- src/main/java/logic/parser/ProfileParser.java
+++ src/main/java/logic/parser/ProfileParser.java
@@ -22,10 +22,22 @@ import static seedu.duke.Constant.COMMAND_WORD_EDIT;
 import static seedu.duke.Constant.COMMAND_WORD_HELP;
 
 //@@author tienkhoa16
+
 /**
  * A class that deals with making sense of user's command inside Profile Session.
  */
 public class ProfileParser extends CommonParser {
+
+    public static final String NAME_SPECIFIER = "/n";
+    public static final String HEIGHT_SPECIFIER = "/h";
+    public static final String WEIGHT_SPECIFIER = "/w";
+    public static final String EXPECTED_WEIGHT_SPECIFIER = "/e";
+    public static final String CALORIES_SPECIFIER = "/c";
+    public static final String SPECIFIER_START_INDICATOR = "/";
+    public static final int CHARACTER_NOT_FOUND_INDEX = -1;
+    public static final int STRING_START_INDEX = 0;
+    public static final int NEXT_INDEX_OFFSET = 1;
+    public static final int PARSED_OPTION_OFFSET = 2;
 
     /**
      * Extracts command tags from user input to get option indicator and parsed option.
@@ -37,24 +49,25 @@ public class ProfileParser extends CommonParser {
      */
     public static HashMap<String, String> extractCommandTagAndInfo(String command, String commandArgs)
             throws SchwarzeneggerException {
-        if (!commandArgs.contains("/")) {
+        if (!commandArgs.contains(SPECIFIER_START_INDICATOR)) {
             throwInvalidCommandFormat(command);
         }
 
         HashMap<String, String> parsedParams = new HashMap<>();
-        int startIndex = 0;
-        int endIndex = 0;
+        int startIndex = STRING_START_INDEX;
+        int endIndex;
 
         try {
-            while (commandArgs.indexOf("/", startIndex) != -1) {
-                endIndex = commandArgs.indexOf("/", startIndex + 1);
+            while (commandArgs.indexOf(SPECIFIER_START_INDICATOR, startIndex) != CHARACTER_NOT_FOUND_INDEX) {
+                endIndex = commandArgs.indexOf(SPECIFIER_START_INDICATOR, startIndex + NEXT_INDEX_OFFSET);
 
-                if (endIndex == -1) {
+                if (endIndex == CHARACTER_NOT_FOUND_INDEX) {
                     endIndex = commandArgs.length();
                 }
 
-                String parsedOption = commandArgs.substring(startIndex + 2, endIndex).trim();
-                String optionIndicator = commandArgs.substring(startIndex, startIndex + 2).trim().toLowerCase();
+                String parsedOption = commandArgs.substring(startIndex + PARSED_OPTION_OFFSET, endIndex).trim();
+                String optionIndicator = commandArgs.substring(startIndex, startIndex + PARSED_OPTION_OFFSET)
+                        .trim().toLowerCase();
                 parsedParams.put(optionIndicator, parsedOption);
 
                 startIndex = endIndex;
@@ -82,14 +95,20 @@ public class ProfileParser extends CommonParser {
 
         switch (command) {
         case COMMAND_WORD_ADD:
-            if (!parsedParams.containsKey("/n") || !parsedParams.containsKey("/h") || !parsedParams.containsKey("/w")
-                    || !parsedParams.containsKey("/e") || !parsedParams.containsKey("/c")) {
+            if (!parsedParams.containsKey(NAME_SPECIFIER)
+                    || !parsedParams.containsKey(HEIGHT_SPECIFIER)
+                    || !parsedParams.containsKey(WEIGHT_SPECIFIER)
+                    || !parsedParams.containsKey(EXPECTED_WEIGHT_SPECIFIER)
+                    || !parsedParams.containsKey(CALORIES_SPECIFIER)) {
                 isSufficient = false;
             }
             break;
         case COMMAND_WORD_EDIT:
-            if (!(parsedParams.containsKey("/n") || parsedParams.containsKey("/h") || parsedParams.containsKey("/w")
-                    || parsedParams.containsKey("/e") || parsedParams.containsKey("/c"))) {
+            if (!(parsedParams.containsKey(NAME_SPECIFIER)
+                    || parsedParams.containsKey(HEIGHT_SPECIFIER)
+                    || parsedParams.containsKey(WEIGHT_SPECIFIER)
+                    || parsedParams.containsKey(EXPECTED_WEIGHT_SPECIFIER)
+                    || parsedParams.containsKey(CALORIES_SPECIFIER))) {
                 isSufficient = false;
             }
             break;
@@ -109,13 +128,16 @@ public class ProfileParser extends CommonParser {
     public static String findInvalidTags(HashMap<String, String> parsedParams) {
         StringBuilder result = new StringBuilder();
 
-        Set<String> validTags = new HashSet<>(Arrays.asList("/n", "/h", "/w", "/e", "/c"));
+        Set<String> validTags = new HashSet<>(Arrays.asList(NAME_SPECIFIER, HEIGHT_SPECIFIER, WEIGHT_SPECIFIER,
+                EXPECTED_WEIGHT_SPECIFIER, CALORIES_SPECIFIER));
         parsedParams.keySet().stream()
                 .filter(tag -> !validTags.contains(tag))
                 .forEach(tag -> result.append(String.format("\"%s\", ", tag)));
 
         String trimmedResult = result.toString().trim();
-        return trimmedResult.isEmpty() ? trimmedResult : trimmedResult.substring(0, trimmedResult.length() - 1);
+        return trimmedResult.isEmpty()
+                ? trimmedResult
+                : trimmedResult.substring(STRING_START_INDEX, trimmedResult.length() - NEXT_INDEX_OFFSET);
     }
 
     /**
@@ -159,7 +181,7 @@ public class ProfileParser extends CommonParser {
      * @throws InvalidNameException If input name is empty.
      */
     public static String extractName(HashMap<String, String> parsedParams) throws InvalidNameException {
-        String name = WordUtils.capitalizeFully(parsedParams.get("/n"));
+        String name = WordUtils.capitalizeFully(parsedParams.get(NAME_SPECIFIER));
 
         if (!Utils.checkValidName(name)) {
             throw new InvalidNameException();
@@ -177,7 +199,7 @@ public class ProfileParser extends CommonParser {
      */
     public static double extractCalories(HashMap<String, String> parsedParams) throws InvalidCaloriesException {
         try {
-            double calories = Double.parseDouble(parsedParams.get("/c"));
+            double calories = Double.parseDouble(parsedParams.get(CALORIES_SPECIFIER));
 
             if (!Utils.checkValidCalories(calories)) {
                 throw new InvalidCaloriesException();
@@ -197,7 +219,7 @@ public class ProfileParser extends CommonParser {
      */
     public static int extractHeight(HashMap<String, String> parsedParams) throws InvalidHeightException {
         try {
-            int height = Integer.parseInt(parsedParams.get("/h"));
+            int height = Integer.parseInt(parsedParams.get(HEIGHT_SPECIFIER));
 
             if (!Utils.checkValidHeight(height)) {
                 throw new InvalidHeightException();
@@ -217,7 +239,7 @@ public class ProfileParser extends CommonParser {
      */
     public static double extractWeight(HashMap<String, String> parsedParams) throws InvalidWeightException {
         try {
-            double weight = Double.parseDouble(parsedParams.get("/w"));
+            double weight = Double.parseDouble(parsedParams.get(WEIGHT_SPECIFIER));
 
             if (!Utils.checkValidWeight(weight)) {
                 throw new InvalidWeightException();
@@ -237,7 +259,7 @@ public class ProfileParser extends CommonParser {
      */
     public static double extractExpectedWeight(HashMap<String, String> parsedParams) throws InvalidWeightException {
         try {
-            double expectedWeight = Double.parseDouble(parsedParams.get("/e"));
+            double expectedWeight = Double.parseDouble(parsedParams.get(EXPECTED_WEIGHT_SPECIFIER));
 
             if (!Utils.checkValidWeight(expectedWeight)) {
                 throw new InvalidWeightException();
